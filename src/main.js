3d872f829ca9d3e797ab1d8f4ab9cdaddbedd9de
Your Real Name <you@email.address>
2018-11-05 14:13:48 +0800

    sss

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './router/router'
-import store from './store/'
+import store from './store'
 import ajax from './config/ajax'
 import './style/common'
 import './config/rem'
 
+
 Vue.use(VueRouter)
 const router = new VueRouter({
-	routes
+  routes
 })
 
 new Vue({
-	router,
-	store,
+  router,
+  store,
 }).$mount('#app')
